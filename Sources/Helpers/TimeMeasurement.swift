2d15a0530f6837a8b73762f6fe37213e2160d326
Chudin Yuriy <chudin26@gmail.com>
2018-07-13 03:25:51 +0300

    Added TimeMeasurement class for measure of how long operation takes

---
+++ Sources/Helpers/TimeMeasurement.swift
@@ -0,0 +1,21 @@
+//
+//  TimeMeasurement.swift
+//  Xelby
+//
+//  Created by Yuriy Chudin on 13/07/2018.
+//  Copyright © 2018 chood. All rights reserved.
+//
+
+public final class TimeMeasurement {
+	
+	@discardableResult
+	public static func measure(entityName: String, block: () -> ()) -> TimeInterval {
+		let startDate = Date()
+		block()
+		let interval = Date().timeIntervalSince(startDate)
+		print("TimeMeasurement: \(entityName) operation takes \(interval) time")
+		
+		return interval
+	}
+
+}
