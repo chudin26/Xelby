78935ceef53f5dd5ba71295ef5b78d3f499e50a6
Prodacle <chudin26@gmail.com>
2020-01-15 18:19:18 +0700

    Methods for mapping keys in dictionary

--- Sources/Foundation/DictionaryExtensions.swift
+++ Sources/Foundation/DictionaryExtensions.swift
@@ -34,4 +34,19 @@ public extension Dictionary {
 		}
 	}
 
+	func compactMapKeys <T> (_ transform: ((Key) throws -> T?)) rethrows -> Dictionary <T, Value> {
+		return try self.reduce(into: [T: Value]()) { (result, x) in
+			if let key = try transform(x.key) {
+				result[key] = x.value
+			}
+		}
+	}
+
+	func mapKeys <T> (_ transform: ((Key) throws -> T)) rethrows -> Dictionary <T, Value> {
+		return try self.reduce(into: [T: Value]()) { (result, x) in
+			let key = try transform(x.key)
+			result[key] = x.value
+		}
+	}
+
 }
