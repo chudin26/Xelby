ad3a73df2afc2ae366f3058bbe3ad0a61d9cfd22
Prodacle <chudin26@gmail.com>
2020-06-16 02:04:57 +0700

    Added loadImageAsync method in UIImageView extension for load image from network

--- Sources/Views/ImageViews/UIImageViewExtensions.swift
+++ Sources/Views/ImageViews/UIImageViewExtensions.swift
@@ -11,11 +11,7 @@ import UIKit
 public extension UIImageView {
 
 	func loadImageAsync(fromFileUrl fileUrl: URL) {
-		let	loadingView = UIActivityIndicatorView(style: .gray)
-		loadingView.frame = bounds
-		loadingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-		loadingView.startAnimating()
-		self.addSubview(loadingView)
+		let	loadingView = makeLoadingView()
 
 		DispatchQueue.global().async {
 			let imageData = UIImage(contentsOfFile: fileUrl.path)
@@ -27,12 +23,35 @@ public extension UIImageView {
 		}
 	}
 	
+	func loadImageAsync(fromUrl url: URL) {
+		let	loadingView = makeLoadingView()
+
+		DispatchQueue.global().async {
+			if let imageData = try? Data(contentsOf: url) {
+				DispatchQueue.main.async {
+					self.image = UIImage(data: imageData)
+					loadingView.removeFromSuperview()
+				}
+			}
+		}
+	}
+	
 	func setImageFromBase64(dataString: String) {
 		if let url = URL(string: dataString),
 			let imageData = try? Data(contentsOf: url),
 			let image = UIImage(data: imageData) {
 			self.image = image
 		}
+	}
+	
+	private func makeLoadingView() -> UIView {
+		let	loadingView = UIActivityIndicatorView(style: .gray)
+		loadingView.frame = bounds
+		loadingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		loadingView.startAnimating()
+		self.addSubview(loadingView)
+		
+		return loadingView
 	}
 
 }
